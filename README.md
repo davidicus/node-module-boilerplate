96bc57274504bebc09af1f064b822dcbc0c57969
davidicus <david@david-conner.com>
2018-10-24 13:02:33 -0500

    commiting initial files

---
+++ README.md
@@ -0,0 +1,5 @@
+# Node Module Boilerplate
+
+---
+
+The basic structure and files for creating node modules. To be used with [kick-init](https://www.npmjs.com/package/kick-init)
---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+/**
+ * This function does something.
+ * @param input Some param description.
+ * @param options An object of options.
+ * @returns something userful.
+ */
+module.exports = (input, options = {}) => {};
